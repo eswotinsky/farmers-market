665d55e2e9ca485448772e0fb25d8f5b0ab303d6
eric swotinsky and tyler kostelak <eric+tyler@>
2018-04-19 14:31:26 -0700

    add Schedule component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Header from './Header';
+import Schedule from './Schedule';
 
 function App(){
   return (
     <div>
       <Header/>
       <Switch>
+        <Route exact path='/' component={Schedule} />
       </Switch>
     </div>
   );
---
+++ src/components/Schedule.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Schedule(){
+  return (
+    <div>
+      <style jsx>{`
+          div {
+            background-color:pink;
+          }
+        `}
+      </style>
+      <p>This is the Schedule component.</p>
+    </div>
+  );
+}
+
+export default Schedule;
